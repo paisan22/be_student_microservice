c2bcba640cbf383b1013ebb2f550dd29086d293d
paisan <paisan.rietbroek@gmail.com>
2017-11-13 14:53:12 +0100

    jenkins test9

--- src/test/java/nl/ipsenh/student/StudentServiceApplicationTests.java
+++ src/test/java/nl/ipsenh/student/StudentServiceApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.validation.constraints.AssertTrue;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -16,6 +18,8 @@ public class StudentServiceApplicationTests {
 	@Test
 	public void contextLoads() {
 		assertTrue(false);
+		assertFalse(true);
+		assertEquals(1, 0);
 	}
 
 }
