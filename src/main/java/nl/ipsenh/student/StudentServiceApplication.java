0f0edd074f90883f17f4471ed36e2a4372272b84
paisan <paisan.rietbroek@gmail.com>
2017-11-13 14:44:05 +0100

    jenkins test6

--- src/main/java/nl/ipsenh/student/StudentServiceApplication.java
+++ src/main/java/nl/ipsenh/student/StudentServiceApplication.java
@@ -9,6 +9,6 @@ public class StudentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServiceApplication.class, args);
-		System.out.println("test5");
+		System.out.println("test6");
 	}
 }
