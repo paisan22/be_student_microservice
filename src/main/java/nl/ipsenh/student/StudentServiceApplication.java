8f275cb8b9256ee39fe6fbc7888d04aeb1121481
paisan <paisan.rietbroek@gmail.com>
2017-11-13 14:49:57 +0100

    jenkins test8

--- src/main/java/nl/ipsenh/student/StudentServiceApplication.java
+++ src/main/java/nl/ipsenh/student/StudentServiceApplication.java
@@ -9,6 +9,5 @@ public class StudentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServiceApplication.class, args);
-		System.out.println(fail);
 	}
 }
--- src/test/java/nl/ipsenh/student/StudentServiceApplicationTests.java
+++ src/test/java/nl/ipsenh/student/StudentServiceApplicationTests.java
@@ -5,12 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.constraints.AssertTrue;
+
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StudentServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(false);
 	}
 
 }
