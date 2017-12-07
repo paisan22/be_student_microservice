9c561041c5b408e622a4ca29107e31d04d0ba8c9
paisan <paisan.rietbroek@gmail.com>
2017-12-07 10:30:37 +0100

    login with generated token created

---
+++ src/main/java/nl/ipsenh/student/API/LoginAPI.java
@@ -0,0 +1,30 @@
+package nl.ipsenh.student.API;
+
+import nl.ipsenh.student.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by paisanrietbroek on 05/12/2017.
+ */
+@RestController
+@CrossOrigin(value = "*")
+public class LoginAPI {
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping(value = "/login")
+    public String login(@RequestHeader("email") String email, @RequestHeader("password") String password) throws UnsupportedEncodingException {
+
+        return loginService.Authententicate(email, password);
+    }
+
+    @GetMapping(value = "/verify")
+    public boolean verifyToken(@RequestHeader("token") String token) throws UnsupportedEncodingException {
+        return loginService.verifyToken(token);
+
+    }
+}
