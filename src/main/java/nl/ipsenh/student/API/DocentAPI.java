20ea1bcf0923018de343756651d262a5afb24a80
paisan <paisan.rietbroek@gmail.com>
2017-12-18 17:47:06 +0100

    create internship proposal finished, but fake data now because of groep 1

--- src/main/java/nl/ipsenh/student/API/DocentAPI.java
+++ src/main/java/nl/ipsenh/student/API/DocentAPI.java
@@ -2,7 +2,13 @@ package nl.ipsenh.student.API;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 
@@ -18,7 +24,9 @@ public class DocentAPI {
     private final String docentAPI = "http://ipsenh.win/";
 
     @PostMapping(value = "/internship_proposal")
-    public HashMap<String, String> createProposal(@RequestBody HashMap<String, String> hashMap) {
+    public JSONObject createProposal(@RequestBody HashMap<String, String> hashMap) {
+
+        String resource = docentAPI + "stagevoorstellen/api/listcreate/";
 
         System.out.println("request: " + hashMap);
 
@@ -27,86 +35,120 @@ public class DocentAPI {
         String proposal = hashMap.get("proposal");
         String stage_id = hashMap.get("stage_id");
 
-        HashMap<String, String> statusHashmap = checkInternshipProposal(hashMap);
+        JSONObject jsonObjectCheck = checkInternshipProposal(hashMap);
 
-        JSONObject jsonObject = new JSONObject();
-        JSONObject begeleiderObject = new JSONObject();
-        begeleiderObject.put("id", mentor_id);
+        if (jsonObjectCheck.get("status").toString() == "true") {
 
-        jsonObject.put("studenten_email", student_email);
+//            HttpHeaders httpHeaders = new HttpHeaders();
+//            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+//            HttpEntity<String> parameters = new HttpEntity<>("parameters", httpHeaders);
+//
+//            try {
+//                RestTemplate restTemplate = new RestTemplate();
+//                ResponseEntity<String> exchange = restTemplate.exchange(resource, HttpMethod.POST, parameters, String.class);
+//
+//                String jsonString = exchange.getBody();
+//                JSONParser jsonParser = new JSONParser();
+//
+//            } catch (HttpClientErrorException e) {
+//                JSONObject jsonObject = new JSONObject();
+//                jsonObject.put("status", "false");
+//                jsonObject.put("message", "docentAPI connection error");
+//                return jsonObject;
+//            }
+//            catch (HttpServerErrorException e) {
+//
+//                JSONObject jsonObject = new JSONObject();
+//                jsonObject.put("status", "false");
+//                jsonObject.put("message", "docentAPI connection error");
+//                return jsonObject;
+//            }
+//
+//            JSONObject jsonObject = new JSONObject();
+//            JSONObject begeleiderObject = new JSONObject();
+//            begeleiderObject.put("id", mentor_id);
+//            begeleiderObject.put("rol", "mentor");
+//
+//            jsonObject.put("studenten_email", student_email);
+//
+//            jsonObject.put("begeleiders", begeleiderObject);
+//            jsonObject.put("stagevoorstel", proposal);
+//            jsonObject.put("stage_id", stage_id);
+//
+//
+//
+//            return jsonObject;
 
-        jsonObject.put("begeleiders", begeleiderObject);
-        jsonObject.put("stagevoorstel", proposal);
-        jsonObject.put("stage_id", stage_id);
+            return jsonObjectCheck;
+        }
 
-        System.out.println(jsonObject);
-
-        return statusHashmap;
+        return jsonObjectCheck;
     }
 
-    public HashMap<String, String> checkInternshipProposal(HashMap<String, String> hashMap) {
+    public JSONObject checkInternshipProposal(HashMap<String, String> hashMap) {
 
-        HashMap<String, String> stringStringHashMap = new HashMap<>();
-        stringStringHashMap.put("status", "true");
-        stringStringHashMap.put("student_email","true");
-        stringStringHashMap.put("mentor_id","true");
-        stringStringHashMap.put("proposal","true");
-        stringStringHashMap.put("stage_id","true");
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("status", "true");
+        jsonObject.put("student_email","true");
+        jsonObject.put("mentor_id","true");
+        jsonObject.put("proposal","true");
+        jsonObject.put("stage_id","true");
 
         if (hashMap.size() != 4) {
-            stringStringHashMap.put("status", "false");
-            stringStringHashMap.put("message", "not all needed keys are available");
+            jsonObject.put("status", "false");
+            jsonObject.put("message", "not all needed keys are available");
         }
 
         if (hashMap.get("student_email").isEmpty()) {
-            stringStringHashMap.put("status", "false");
-            stringStringHashMap.put("student_email","false");
+            jsonObject.put("status", "false");
+            jsonObject.put("student_email","false");
         }
 
         if (hashMap.get("mentor_id").isEmpty()) {
-            stringStringHashMap.put("status", "false");
-            stringStringHashMap.put("mentor_id","false");
+            jsonObject.put("status", "false");
+            jsonObject.put("mentor_id","false");
 
         }
 
         if (hashMap.get("proposal").isEmpty()) {
-            stringStringHashMap.put("status", "false");
-            stringStringHashMap.put("proposal","false");
+            jsonObject.put("status", "false");
+            jsonObject.put("proposal","false");
 
         }
 
         if (hashMap.get("stage_id").isEmpty()) {
-            stringStringHashMap.put("status", "false");
-            stringStringHashMap.put("stage_id","false");
+            jsonObject.put("status", "false");
+            jsonObject.put("stage_id","false");
 
         }
 
-        return stringStringHashMap;
+        return jsonObject;
 
     }
 
-    @GetMapping
-    public JSONArray getAllDocents() {
-        JSONArray jsonArray = new JSONArray();
-
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("id", "123");
-        jsonObject.put("name", "Alex van Manen");
-
-        JSONObject jsonObject1 = new JSONObject();
-        jsonObject1.put("id", "456");
-        jsonObject1.put("name", "Roland Westveer");
-
-        JSONObject jsonObject2 = new JSONObject();
-        jsonObject2.put("id", "456");
-        jsonObject2.put("name", "Michiel Boerre");
-
-        jsonArray.add(jsonObject);
-        jsonArray.add(jsonObject1);
-        jsonArray.add(jsonObject2);
-
-        return jsonArray;
-    }
+//    @GetMapping
+//    public JSONArray getAllDocents() {
+//        JSONArray jsonArray = new JSONArray();
+//
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("id", "123");
+//        jsonObject.put("name", "Alex van Manen");
+//
+//        JSONObject jsonObject1 = new JSONObject();
+//        jsonObject1.put("id", "456");
+//        jsonObject1.put("name", "Roland Westveer");
+//
+//        JSONObject jsonObject2 = new JSONObject();
+//        jsonObject2.put("id", "456");
+//        jsonObject2.put("name", "Michiel Boerre");
+//
+//        jsonArray.add(jsonObject);
+//        jsonArray.add(jsonObject1);
+//        jsonArray.add(jsonObject2);
+//
+//        return jsonArray;
+//    }
 
     @GetMapping(value = "/email")
     public JSONObject getDocentByEmail(@RequestHeader HashMap<String, String> hashMap) {
@@ -114,40 +156,40 @@ public class DocentAPI {
     }
 
 
-    //    @GetMapping
-//    public JSONArray getAllDocents() {
-//
-//        String resource = docentAPI + "docenten/api/list";
-//
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-//        HttpEntity<String> parameters = new HttpEntity<>("parameters", httpHeaders);
-//
-//        try {
-//            RestTemplate restTemplate = new RestTemplate();
-//            ResponseEntity<String> exchange = restTemplate.exchange(resource, HttpMethod.GET, parameters, String.class);
-//
-//            String jsonString = exchange.getBody();
-//            JSONParser jsonParser = new JSONParser();
-//            Object parse = jsonParser.parse(jsonString);
-//
-//            return (JSONArray) parse;
-//
-//        } catch (HttpClientErrorException e) {
-//            return null;
-//        }
-//        catch (HttpServerErrorException e) {
-//            System.out.println(e.getMessage());
-//            JSONArray jsonArray = new JSONArray();
-//            JSONObject jsonObject = new JSONObject();
-//            jsonObject.put("error", e.getMessage());
-//            jsonArray.add(jsonObject);
-//            return jsonArray;
-//        }
-//        catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//
-//    return null;
-//    }
+    @GetMapping
+    public JSONArray getAllDocents() {
+
+        String resource = docentAPI + "docenten/api/list";
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> parameters = new HttpEntity<>("parameters", httpHeaders);
+
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> exchange = restTemplate.exchange(resource, HttpMethod.GET, parameters, String.class);
+
+            String jsonString = exchange.getBody();
+            JSONParser jsonParser = new JSONParser();
+            Object parse = jsonParser.parse(jsonString);
+
+            return (JSONArray) parse;
+
+        } catch (HttpClientErrorException e) {
+            return null;
+        }
+        catch (HttpServerErrorException e) {
+            System.out.println(e.getMessage());
+            JSONArray jsonArray = new JSONArray();
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("error", e.getMessage());
+            jsonArray.add(jsonObject);
+            return jsonArray;
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    return null;
+    }
 }
